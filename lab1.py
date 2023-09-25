426e0276ad6c908c7032c19beba4e6a8a327d5da
Dami-Zuk <130228499+Dami-Zuk@users.noreply.github.com>
2023-09-25 20:47:22 +0300

    Add files via upload
    Homework from school, part 1

---
+++ lab1.py
@@ -0,0 +1,15 @@
+title = 'Fill in the correct forms.'
+keyword = 'coding'
+words = ['oct', 'float', 'date', 'input', 'print', 'program']
+explanations = ['Integer number expressed with base 8', 'Decimal point number',
+                'Python library for handling dates', 'Built-in function to read user\'s input to the program',
+                'Prints text to console', 'Set of operations executed together']
+
+print(' '*34, title, '\n', '_'*100)
+print(explanations[0], ' '*40 + '_', words[0][1].upper(), '_')
+print(explanations[1], ' '*55 + '_'*2, words[1][2].upper(), '_'*2)
+print(explanations[2], ' '*44, words[2][0].upper(), '_'*3)
+print(explanations[3], ' '*24, words[3][0].upper(), '_'*4)
+print(explanations[4], ' '*52 + '_'*3, words[4][3].upper(), '_')
+print(explanations[5], ' '*39 + '_'*3, words[5][3].upper(), '_'*3)
+
